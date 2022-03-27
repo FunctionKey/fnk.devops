03a4b12102b8ffee2380dda7781bd83608b18c68
Filipe <filipenepopereira@gmail.com>
2022-03-27 15:49:16 +0100

    first commit

---
+++ docker-install.sh
@@ -0,0 +1,34 @@
+## https://docs.docker.com/engine/install/ubuntu/
+
+# Uninstall old versions
+sudo apt remove docker docker-engine docker.io containerd runc
+
+## Install using the repository
+# Update the apt package index to allow apt to use a repository over HTTPS
+sudo apt update
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+# Initialize Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+# verify that docker if running
+docker container ls
+
+# Use hello-world image to verify that Docker Engine is installed correctly
+sudo docker run hello-world
