010c37c07442f7f7017a85248b660e7310cba895
Filipe <filipenepopereira@gmail.com>
2022-03-27 16:13:19 +0100

    add bash.sh

---
+++ bash.sh
@@ -0,0 +1,15 @@
+# Check which shell you are using
+which $SHELL
+# Ex: /bin/bash
+
+# Start every bash file with a Shebang
+# #!/bin/bash
+
+# File has to be saved with .sh extention
+
+# Add execute permission to file
+chmod +x file.sh
+
+# Run bash file
+bash file.sh
+./file.sh
