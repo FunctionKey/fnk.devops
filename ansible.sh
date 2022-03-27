47f5c63768caefe26a976d5951724473aaf9c2cc
Filipe <filipenepopereira@gmail.com>
2022-03-27 22:06:45 +0100

    add kubernetes-install.sh

--- ansible.sh
+++ ansible.sh
@@ -3,3 +3,19 @@ sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+
+# Configure
+/etc/ansible/hosts
+"[k8s-cluster]
+
+192.168.4.20
+192.168.4.21
+192.168.4.22
+
+[k8s-cluster:vars]
+
+ansible_user=sam
+ansible_password=P@assword"
+
+/etc/ansible/ansible.cfg
+# uncomment SSH key
---
+++ kubernetes-install.sh
@@ -0,0 +1,14 @@
+# Install docker before installing Kubernetes
+
+# Install dependencies on each ubuntu system
+sudo apt install -y apt-transport-https curl
+
+# Add Kubernetes GPG key on each ubuntu system
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# expect OK from terminal
+
+# Add Kubernetes repository on each ubuntu system
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+# Actualy install kubernetes
+sudo apt install -y kubeadm kubelet kubectl
