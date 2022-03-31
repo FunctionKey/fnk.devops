83c229664838fc71d9099c881471919159584d08
Filipe <filipenepopereira@gmail.com>
2022-03-31 01:35:11 +0100

    tests

--- kubernetes-use.sh
+++ kubernetes-use.sh
@@ -2,9 +2,6 @@
 sudo kubeadm version && kubelet --version && kubectl version
 
 # Initialize kubernetes [ON MASTER]
-IPADDR="10.0.0.10"  # replace ip with master-node's ip
-NODENAME=$(hostname -s)
-
 # Create config file [ON MASTER]
 sudo mkdir ~/.kube
 sudo cp /etc/kubernetes/admin.conf ~/.kube/
@@ -12,10 +9,22 @@ cd ~/.kube
 sudo mv admin.conf config
 sudo service kubelet restart
 
-# Initialize Kubernetes [ON MASTER]
-sudo kubeadm init \
-    --apiserver-advertise-address=$IPADDR  \
-    --apiserver-cert-extra-sans=$IPADDR  \
-    --pod-network-cidr=192.168.0.0/16 \
-    --node-name $NODENAME \
-    #--ignore-preflight-errors Swap   # not required if disabled in kubernetes-install.sh
+# 2 ways of initializing k8s. In testing fase
+    # Initialize Kubernetes [ON MASTER]
+    IPADDR="10.0.0.10"  # replace ip with master-node's ip
+    NODENAME=$(hostname -s)
+    sudo kubeadm init \
+        --apiserver-advertise-address=$IPADDR  \
+        --apiserver-cert-extra-sans=$IPADDR  \
+        --pod-network-cidr=192.168.0.0/16 \
+        --node-name $NODENAME #\
+        #--ignore-preflight-errors Swap   # not required if disabled in kubernetes-install.sh
+
+
+    # Expose Kubernetes API [ON MASTER]
+    # Use the internal IP address of the master node in the flag --apiserver-advertise-address=
+    kubeadm init \
+        --pod-network-cidr=10.244.0.0/16 \
+        --apiserver-advertise-address=10.0.0.4 \
+        --skip-preflight-checks \
+        --kubernetes-version stable-1.23
