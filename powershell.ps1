7617870d7ffef416845886eff827ec63a0365e9c
Filipe <filipenepopereira@gmail.com>
2022-04-18 10:19:14 +0100

    update powershell

--- powershell.ps1
+++ powershell.ps1
@@ -8,3 +8,17 @@ $PSVersionTable
 $env:PSModulePath -split (';')
 
 # Load Windows PowerShell modules into PowerShell v7 by invokng the -UseWindowsPowerShell switch parameter of the Import-Module command
+
+#Region Windows Server
+# Network
+Set-NetAdapterBinding -Name * -ComponentID ms_tcpip6 -Enabled $false
+Set-NetConnectionProfile -InterfaceAlias * -NetworkCategory Private # Or Domain or Public
+
+# Setup Domain Controller
+Get-WindowsFeature
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+# Install-Module ActiveDirectory (included in management tools)
+Test-ADDSForestInstallation -DomainName "fnk.lab.local" -InstallDns
+Install-ADDSForest -DomainName "fnk.lab.local" -InstallDNS
+
+#endregion
